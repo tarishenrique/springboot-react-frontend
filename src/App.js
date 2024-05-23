a6a8c3a31dca0595c68478caf78c2639cf1c7169
Taris Henrique <taris.tj@hotmail.com>
2024-05-23 00:28:01 -0300

    feature: Implementada a tabela que consome os dados do backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import EstudanteView from './component/estudante/EstudanteView';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Bem Vindo ao nosso FrontEnd</h2>
+      <Home />
+      <EstudanteView />
     </div>
   );
 }
---
+++ src/component/estudante/EstudanteView.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const EstudanteView = () => {
+  
+    const [estudantes, setEstudantes] = useState([]);
+
+  useEffect(() => {
+    loadEstudantes();
+  }, []);
+
+  const loadEstudantes = async () => {
+    const result = await axios.get("http://localhost:9192/estudantes", {
+        validateStatus: () => {
+            return true;
+        }
+    });
+    if(result.status === 302){
+        setEstudantes(result.data);
+    }
+  };
+
+  return (
+    <section>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Primeiro Nome</th>
+            <th>Ultimo Nome</th>
+            <th>Email</th>
+            <th>Departamento</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {estudantes.map((estudante, index) => (
+            <tr key={estudante.estudanteId}>
+              <th scope="row" key={index}>
+                {index + 1}
+              </th>
+              <td>{estudante.primeiroNome}</td>
+              <td>{estudante.ultimoNome}</td>
+              <td>{estudante.email}</td>
+              <td>{estudante.departamento}</td>
+              <td>Visualizar</td>
+              <td>Editar</td>
+              <td>Excluir</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default EstudanteView;
